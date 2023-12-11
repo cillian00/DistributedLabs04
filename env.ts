d195b748725654d4bbd9f5dace26f629c7bab70b
cillian00 <20094972@mail.wit.ie>
2023-12-11 13:00:10 +0000

    Completed Fan out implementation - added emailing logic!

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = 'eu-west-1';
+export const SES_EMAIL_FROM = 'cillian0murphy@gmail.com' ;
+export const SES_EMAIL_TO =  'cillian0murphy@gmail.com';
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -30,11 +30,22 @@ export class EDAAppStack extends cdk.Stack {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
     });
 
     // Lambda functions
+
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
 
     const processImageFn = new lambdanode.NodejsFunction(
         this,
@@ -58,16 +69,34 @@ export class EDAAppStack extends cdk.Stack {
     newImageTopic.addSubscription(
         new subs.SqsSubscription(imageProcessQueue)
     );
+
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    });
     const newImageEventSource = new events.SqsEventSource(imageProcessQueue , {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(10),
     });
 
     processImageFn.addEventSource(newImageEventSource);
-
+    mailerFn.addEventSource(newImageMailEventSource);
     // Permissions
 
     imagesBucket.grantRead(processImageFn);
+    mailerFn.addToRolePolicy(
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: [
+            "ses:SendEmail",
+            "ses:SendRawEmail",
+            "ses:SendTemplatedEmail",
+          ],
+          resources: ["*"],
+        })
+    );
 
     // Output
 
